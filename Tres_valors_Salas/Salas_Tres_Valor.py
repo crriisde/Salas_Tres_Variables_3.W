9d7d436fdf4ec02c2f40c701ee7253ba1a82c61b
crriisde <a.23308051281214@cbtis128.edu.mx>
2024-09-26 16:20:22 -0600

    Add files via upload

---
+++ Tres_valors_Salas/Salas_Tres_Valor.py
@@ -0,0 +1,14 @@
+print ("Salas De La O Cristian David 3-W 1214") #esta linea muestra el nombre del alumno que realizo este programa
+A = int(input("Ingrese el valor de A: ")) #esta linea define la variable "A"
+B = int(input("Ingrese el valor de B: ")) #esta linea define la variable "B"
+C = int(input("Ingrese el valor de C: ")) #esta linea define la variable "C"
+
+if A == B or A == C or B == C: #esta linea define que hacer si las variables son iguales
+    print("Alerta Los valores deben ser distintos") #esta es la alerta de los valores si son iguales
+else: #esta linea es para que hacer si no se cumple con lo solicitado
+    
+    mayor = max(A, B, C) #esta linea dice como saber cual es el numero maximo 
+    menor = min(A, B, C) #esta linea dice como saber cual es el numero menor
+    
+    print(f"El mayor es: {mayor}") #esta linea dice que mostrar si el numero es el mayor
+    print(f"El menor es: {menor}") #esta linea dice que mostrar si el numero es el menor
